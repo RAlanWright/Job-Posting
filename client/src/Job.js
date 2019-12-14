62cfa529a392f6b810d3b8cfeb76c77e0b7e6a3f
RAlanWright <wright.alan88@gmail.com>
2019-12-14 14:15:39 -0600

    Built out api; Added styling to App.css; built out worker tasks to filter job list; Yarn installed material icons

--- client/src/Job.js
+++ client/src/Job.js
@@ -1,11 +1,23 @@
 import React from "react";
+import Paper from "@material-ui/core/Paper";
 import { Typography } from "@material-ui/core";
 
 export default function Jobs({ job }) {
   return (
-    <div className={"job"}>
-      <Typography>{job.title}</Typography>
-      <Typography>{job.company}</Typography>
-    </div>
+    <Paper className={"job"}>
+      <div>
+        <Typography variant="h5">{job.title}</Typography>
+        <Typography variant="h6">{job.company}</Typography>
+        <Typography>{job.location}</Typography>
+      </div>
+      <div>
+        <Typography>
+          {job.created_at
+            .split(" ")
+            .slice(0, 3)
+            .join(" ")}
+        </Typography>
+      </div>
+    </Paper>
   );
 }
--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -1,15 +1,63 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Dialog from "@material-ui/core/Dialog";
+import Button from "@material-ui/core/Button";
+
+import MobileStepper from "@material-ui/core/MobileStepper";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
 
 import Job from "./Job";
 
 export default function Jobs({ jobs }) {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(prevActiveStep => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(prevActiveStep => prevActiveStep - 1);
+  };
+
+  console.log("job is", jobs[0]);
+
   return (
     <div className="jobs">
-      <Typography variant="h1">Entry Level Software Jobs</Typography>
-      {jobs.map(job => (
-        <Job job={job} />
+      <Typography variant="h4" component="h1">
+        Entry Level Software Jobs
+      </Typography>
+      {jobs.map((job, i) => (
+        <Job key={i} job={job} />
       ))}
+
+      <MobileStepper
+        variant="progress"
+        steps={6}
+        position="static"
+        activeStep={activeStep}
+        nextButton={
+          <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
+            Next
+            {direction === "rtl" ? (
+              <KeyboardArrowLeft />
+            ) : (
+              <KeyboardArrowRight />
+            )}
+          </Button>
+        }
+        backButton={
+          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
+            {theme.direction === "rtl" ? (
+              <KeyboardArrowRight />
+            ) : (
+              <KeyboardArrowLeft />
+            )}
+            Back
+          </Button>
+        }
+      />
     </div>
   );
 }
