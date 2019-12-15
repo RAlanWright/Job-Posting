fa7043499361473c22e395b950b681bca0a7fc15
RAlanWright <wright.alan88@gmail.com>
2019-12-15 11:12:40 -0600

    Created JobModal.js to handle click events when selecting a job; Styled the site to better display each posting.

---
+++ README.md
@@ -0,0 +1,17 @@
+# Job Postings
+
+This is a React + NodeJS application that grabs data from job-posting website(s).
+
+## Local Testing
+
+Terminal directory of /client:
+
+    yarn start
+
+The above will be the main locally hosted site.
+
+Top level directory:
+
+    node api/index.js
+
+The above will build/grab data from the db.
--- client/src/Jobs.js
+++ client/src/Jobs.js
@@ -1,17 +1,32 @@
 import React from "react";
 import Typography from "@material-ui/core/Typography";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import Dialog from "@material-ui/core/Dialog";
+import Job from "./Job";
+import JobModal from "./JobModal";
 import Button from "@material-ui/core/Button";
-
 import MobileStepper from "@material-ui/core/MobileStepper";
 import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
 import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
 
-import Job from "./Job";
-
 export default function Jobs({ jobs }) {
+  // Modal
+  const [open, setOpen] = React.useState(false);
+  const [selectedJob, selectJob] = React.useState({});
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  // Pagination
+  const numJobs = jobs.length;
+  const numPages = Math.ceil(numJobs / 50);
   const [activeStep, setActiveStep] = React.useState(0);
+  const jobsOnPage = jobs.slice(activeStep * 50, activeStep * 50 + 50);
+
+  // step == 0, show 0-49
+  // step == 1, show 50-199
 
   const handleNext = () => {
     setActiveStep(prevActiveStep => prevActiveStep + 1);
@@ -25,36 +40,47 @@ export default function Jobs({ jobs }) {
 
   return (
     <div className="jobs">
+      <JobModal open={open} job={selectedJob} handleClose={handleClose} />
       <Typography variant="h4" component="h1">
         Entry Level Software Jobs
       </Typography>
-      {jobs.map((job, i) => (
-        <Job key={i} job={job} />
+      <Typography variant="h6" component="h2">
+        Found {numJobs} Jobs
+      </Typography>
+      {jobsOnPage.map((job, i) => (
+        <Job
+          key={i}
+          job={job}
+          onClick={() => {
+            // console.log("clicked");
+            handleClickOpen();
+            selectJob(job);
+          }}
+        />
       ))}
 
+      <div>
+        Page {activeStep + 1} of {numPages}
+      </div>
       <MobileStepper
         variant="progress"
-        steps={6}
+        steps={numPages}
         position="static"
         activeStep={activeStep}
         nextButton={
-          <Button size="small" onClick={handleNext} disabled={activeStep === 5}>
+          <Button
+            size="small"
+            onClick={handleNext}
+            disabled={activeStep === numPages - 1}
+          >
             Next
-            {direction === "rtl" ? (
-              <KeyboardArrowLeft />
-            ) : (
-              <KeyboardArrowRight />
-            )}
+            <KeyboardArrowRight />
           </Button>
         }
         backButton={
           <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {theme.direction === "rtl" ? (
-              <KeyboardArrowRight />
-            ) : (
-              <KeyboardArrowLeft />
-            )}
             Back
+            <KeyboardArrowLeft />
           </Button>
         }
       />
